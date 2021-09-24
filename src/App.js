8404b84b0d60115e4250a859074d7359d4444e46
rpadre <richardmpadre@gmail.com>
2021-09-24 09:52:08 -0400

    init changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
+import logo from './images/samgyup-kita-logo.png';
+import menu1 from './images/menu-1.jpg';
+import menu2 from './images/menu-2.jpg';
+import menu3 from './images/menu-3.jpg';
+import SimpleImageSlider from "react-simple-image-slider";
+import Slider from 'react-touch-drag-slider'
 import './App.css';
 
+const images = [
+  { url: menu1, title: 'Menu1' },
+  { url: menu2, title: 'Menu2' },
+  { url: menu3, title: 'Menu3' },
+];
+
+
 function App() {
+  console.log(logo);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <SimpleImageSlider
+          width={614}
+          height={1024}
+          showNavs={true}
+          showBullets={true}
+          images={images}
+        />
+
+        {/* <Slider
+          onSlideComplete={(i) => {
+            console.log('finished dragging, current slide is', i)
+          }}
+          onSlideStart={(i) => {
+            console.log('started dragging on slide', i)
+          }}
+          activeIndex={0}
+          threshHold={100}
+          transition={0.5}
+          scaleOnDrag={true}
         >
-          Learn React
-        </a>
+          {images.map(({ url, title }, index) => (
+            <img src={url} key={index} alt={title} />
+          ))}
+        </Slider> */}
+
       </header>
-    </div>
+    </div >
   );
 }
 
